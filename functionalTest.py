2652ef341433a6dedbdc205eab51c0f0531c7223
Karthigaa <karthigaa.m61@wipro.com>
2018-04-23 11:42:04 +0530

    Commit

--- functionalTest.py
+++ functionalTest.py
@@ -1,11 +1,11 @@
-import os 
+import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options  
+from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
-chrome_options.add_argument("--headless") 
-driver = webdriver.Chrome(executable_path='/usr/local/share/chromedriver.exe',   chrome_options=chrome_options)
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(executable_path='chromedriver.exe',   chrome_options=chrome_options)
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
