2bc99fa764f23d69a5ce37485bf136e7aa8ca7d1
Navandeep025 <navandeepkaur25@gmail.com>
2018-04-23 12:01:41 +0530

    Update functionalTest.py

--- functionalTest.py
+++ functionalTest.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
-chrome_options.setBinary("/usr/bin/google-chrome-stable")
+#chrome_options.setBinary("/usr/bin/google-chrome-stable")
 #options.binary_location = "/usr/bin/google-chrome-stable"
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(executable_path=r"chromedriver",   chrome_options=chrome_options)
