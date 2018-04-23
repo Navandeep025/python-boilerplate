bd5ce670c2759c24907f6265e7a57e9277997830
Navandeep025 <navandeepkaur25@gmail.com>
2018-04-23 10:18:19 +0530

    Update functionalTest.py

--- functionalTest.py
+++ functionalTest.py
@@ -1,9 +1,9 @@
 import os 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options  
 
-driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"), chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.path.abspath(“chromedriver"),   chrome_options=chrome_options)
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
