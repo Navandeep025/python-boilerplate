d10d4c296adec0c81d0abe7b6d5b19de6fdaa853
Navandeep025 <navandeepkaur25@gmail.com>
2018-04-24 14:37:21 +0530

    Update functionalTest.py

--- functionalTest.py
+++ functionalTest.py
@@ -9,10 +9,10 @@ chrome_options =Options()
 options = webdriver.ChromeOptions()
 #chrome_options=chrome_options
 #chrome_options.setBinary("/usr/bin/google-chrome-stable")
-options.binary_location = "//usr//bin//google-chrome-stable"
+options.binary_location = "/usr/bin/google-chrome-stable"
 options.add_argument("--headless")
 #executable_path=r"chromedriver.exe"
-driver = webdriver.Chrome(executable_path="//usr//local//share//chromedriver.exe")
+driver = webdriver.Chrome(executable_path="/usr/local/share/chromedriver.exe")
 driver.set_page_load_timeout(30)
 driver.get("http://www.python.org")
 driver.implicitly_wait(20)
