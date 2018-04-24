714d615562de31a9d1000669db4b10645efa8af2
Navandeep025 <navandeepkaur25@gmail.com>
2018-04-24 14:28:30 +0530

    Update functionalTest.py

--- functionalTest.py
+++ functionalTest.py
@@ -10,18 +10,13 @@ options = webdriver.ChromeOptions()
 #chrome_options=chrome_options
 #chrome_options.setBinary("/usr/bin/google-chrome-stable")
 options.binary_location = "//usr//bin//google-chrome-stable"
-chrome_options.add_argument("--headless")
+options.add_argument("--headless")
 #executable_path=r"chromedriver.exe"
 driver = webdriver.Chrome("//usr//local//share//chromedriver.exe")
 driver.set_page_load_timeout(30)
 driver.get("http://www.python.org")
 driver.implicitly_wait(20)
 diver.get_screenshot_as_file("screenshot.png")
-# assert "Python" in driver.title
-# elem = driver.find_element_by_name("q")
-# elem.clear()
 
-# elem.send_keys("pycon")
-# elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
+
 driver.quit()
